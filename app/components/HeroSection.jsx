d8d1fdf9ef17a446f2e0b24eb79e4dfed5e0d6fd
Giovanni Bernardo <canaltamojuntooooo@gmail.com>
2024-05-01 10:10:26 -0300

    feat: started a new next.js project

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center">
+          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+            Hi, I am Giovanni
+          </h1>
+          <p className="text-[#ADB7BE] text-lg lg:text-xl">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus, dolores.
+          </p>
+        </div>
+        <div className="col-span-5 place-self-center">
+          <div className="rounded-full bg-[#31373f] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/avatar.png"
+              alt="Giovanni's Pixar photo AI generated - hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              height={300}
+              width={300}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
