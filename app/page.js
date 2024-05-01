5c32dce66f7e25a8c7b336f2d93f18abf96c5b8b
Giovanni Bernardo <canaltamojuntooooo@gmail.com>
2024-05-01 12:59:19 -0300

    feat: added navbar logo and list, updated website title and description

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
-import HeroSection from "./components/HeroSection";
+import HeroSection from "./components/HeroSection.jsx";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#22272E]">
+      <Navbar />
       <div className="container mx-auto px-12 py-4">
         <HeroSection />
       </div>
