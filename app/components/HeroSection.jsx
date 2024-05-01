4e6434960fd998fd782ec673d5d25ad676e317cb
Giovanni Bernardo <canaltamojuntooooo@gmail.com>
2024-05-01 10:56:21 -0300

    feat: created styled buttons and a styled h1

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -5,15 +5,27 @@ const HeroSection = () => {
   return (
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            Hi, I am Giovanni
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl  lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-br from-green-600 via-green-400 to-green-200">
+              Hi, I am{" "}
+            </span>
+             Giovanni
           </h1>
-          <p className="text-[#ADB7BE] text-lg lg:text-xl">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus, dolores.
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Temporibus, dolores.
           </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-green-600 via-green-400 to-green-200 hover:bg-slate-200 text-white font-bold">
+            LinkedIn
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-transparent bg-gradient-to-br from-green-600 via-green-400 to-green-200 hover:bg-slate-800 text-white mt-3 font-bold">
+              <span className="block bg-[#22272E] hover:bg-[#31373f] rounded-full px-5 py-2"> Download CV </span>
+            </button>
+          </div>
         </div>
-        <div className="col-span-5 place-self-center">
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#31373f] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/avatar.png"
--- app/page.js
+++ app/page.js
@@ -3,8 +3,10 @@ import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#22272E] container mx-auto px-12 py-4">
-      <HeroSection />
+    <main className="flex min-h-screen flex-col bg-[#22272E]">
+      <div className="container mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
   );
 }
