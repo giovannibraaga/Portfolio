87f3e776a9ede2328f14c190f11f0765e30ed812
Giovanni Bernardo <canaltamojuntooooo@gmail.com>
2024-05-10 14:00:59 -0300

    fix: fixing the emails issue 2.0

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend('re_88LNLenS_EEQaQzNbkpthYw6kFG1trtQ2');
+const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
@@ -21,8 +21,8 @@ export async function POST(req, res) {
         </>
       ),
     });
-    return NextResponse.json(data);
+    return Response.json(data);
   } catch (error) {
-    return NextResponse.json({ error });
+    return Response.json({ error });
   }
 }
